c920e8e6cddcfb5d8b09b5b50e2231459512a38b
Xavih830 <120606644+Xavih830@users.noreply.github.com>
2025-01-15 16:03:50 -0500

    Update TestOperations.java

--- TestOperations.java
+++ TestOperations.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Pruebas de la clase Operaciones")
 public class TestOperations {
   
   public OperationsTest() {
@@ -27,6 +28,7 @@ public class TestOperations {
   }
 
   @Test
+  @DisplayName("Prueba del método MakeFormula")
   public void testMakeFormula() {
     System.out.println("MakeFormula");
     String expResult = "";
@@ -36,6 +38,7 @@ public class TestOperations {
   } 
 
   @Test
+  @DisplayName("Prueba del método Solve")
   public void testSolve() {
     System.out.println("Solve");
     String formula = "";
@@ -45,5 +48,60 @@ public class TestOperations {
     fail("The test case is a prototype.");
   }
 
-  
+  @Test
+  @DisplayName("Prueba de que formula cumpla con su manera de verse")
+  public void testFormulaFormat(){
+    String formula = MakeFormula();
+    assertTrue(formula.matches("\\d+[\\+\\-\\*/]\\d+([\\+\\-\\*/]\\d+)?"));
+  }
+
+  @Test
+  @DisplayName("Prueba de que formula cumpla con un resultado en el rango esperado")
+  public void testFormulaNumberRange(){
+    String formula = MakeFormula();
+    String[] numbers = formula.split("[\\\\+\\\\-\\\\*/]");
+    for(String number : numbers){
+      int num = Integer.parseInt(number);
+      assertTrue(num >= 1 && num <=99);
+    }
+  }
+
+  @Test
+  @DisplayName("Prueba de que formula cumpla con un número de operadores esperado")
+  public void testFormulaOperatorCount(){
+    String formula = MakeFormula();
+    int operatorCount = formula.replaceAll("[^\\+\\-\\*/]", "").length();
+    assertTrue(operatorCount == 1 || operatorCount == 2);
+  }
+
+  @Test
+  @DisplayName("Prueba de que Solve cumpla con los estandares de suma")
+  public void testSolveSum(){
+    String result = Solve("10+10");
+    assertEquals("10+10=20", result);
+  }
+
+  @Test
+  @DisplayName("Prueba de que Solve cumpla con los estandares de resta")
+  public void testSolveRes(){
+    String result = Solve("10-10");
+    assertEquals("10-10=0", result);
+  }
+
+  @Test
+  @DisplayName("Prueba de que Solve cumpla con los estandares de multiplicación")
+  public void testSolveMul(){
+    String result = Solve("10*10");
+    assertEquals("10*10=100", result);
+  }
+
+  @Test
+  @DisplayName("Prueba de que Solve cumpla con los estandares de división")
+  public void testSolveDiv(){
+    String result = Solve("10/10");
+    assertEquals("10/10=1", result);
+  }
+
+  @Test
+  @DisplayName("Prueba de que Solve cumpla con los estandares de ")
 }
